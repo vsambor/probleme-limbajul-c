ef767eaa5088727a0de709bda72ab5b761640ce4
Vasile Sambor <vasilesambor@gmail.com>
2019-05-14 22:20:16 +0200

    Add problem 1

---
+++ p1.c
@@ -0,0 +1,27 @@
+//  Probleme HubIT | # 2 | Începator
+//
+//  CERINȚĂ:
+//  Să se afișeze pe ecran suma a două numere întregi citite de la tastatuă.
+//
+//  @autor - Vasile Sambor
+
+#include <stdio.h>
+
+int main() {
+  printf("# PROBLEMA 1 \n\n");
+  
+  int numar1;
+  int numar2;
+  
+  printf("Introduceti primul numar: ");
+  scanf("%d", &numar1);
+  
+  printf("Introduceti al doilea numar: ");
+  scanf("%d", &numar2);
+  
+  int suma = numar1 + numar2;
+  
+  printf("\nSuma este: %d\n\n", suma);
+  
+  return 0;
+}
